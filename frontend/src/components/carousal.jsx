02e6c13b6f5b9c588707711a1d5d14d2414162bd
Ali Gauhar <aligauharcb@gmail.com>
2024-03-18 00:23:21 +0500

    Update carousal.jsx

--- frontend/src/components/carousal.jsx
+++ frontend/src/components/carousal.jsx
@@ -6,6 +6,9 @@ function MyCarousel() {
     return (
         <Carousel>
             <Carousel.Item>
+
+                <h3 className='carousal-heading'>Defi Quant - Decentralized Investor Plateform <br/>( MERN + SERN + Firebase ) </h3>
+
                 <div class="card-deck">
                     <div class="card col-md-3">
                         <img className='carousal-img' src="Quant-1.PNG" alt="" />
@@ -24,12 +27,13 @@ function MyCarousel() {
                     </div>
                 </div>
 
-
-
-
-
-                <h3 className='carousal-heading'>Defi Quant - Decentralized Investor Plateform</h3>
-                <p>A projects is based on a business idea of show casing the Quant Trading bot strategies and history The website aims to show the historic success and profit made by the financial bots and attract user investment The user can buy portfolio management on his investment by paying some fees, payment integration Is being done The user will have to open the forex or crypto account and give the API or credentials  Through API account data will be sent to the software where bots are being running to copy the trades of bots</p>
+                <p>This projects is based on a business idea of attracting client to rent bots show casing the Quant Trading bot strategies and success history,
+                    allowing them to register credentials and sending them to bots copiers, the user can buy portfolio management on his investment by paying some fees, 
+                    the user will have to open the forex or crypto account and give the API or credentials, through API account data will be sent to the software where bots
+                    are being running to copy the trades of bots, Currently working on it (components left:  data sending to financial bots with API, payment through different 
+                    option), Functionalities done: google auth, Getting user choice for bot’s selection in DB, Getting, data secrets from backend securely, restricting to frontend 
+                    URL, Beautiful Frontend.
+                </p>
 
                   <a href="https://quant-trading-bots-mern.vercel.app/" target='_blank'>
                 <button>
@@ -38,6 +42,9 @@ function MyCarousel() {
                 </a>
             </Carousel.Item>
             <Carousel.Item>
+
+                <h3 className='carousal-heading'>Ai Law Assistant <br/> ( Django + SQLite )</h3>
+                
                 <div class="card-deck">
                     <div class="card col-md-3">
                         <img className='carousal-img' src="law-assistant1.PNG" alt="" />
@@ -55,14 +62,12 @@ function MyCarousel() {
                         <img className='carousal-img' src="law-assistant3.PNG" alt="" />
                     </div>
                 </div>
-
-
-
-
-
-                <h3 className='carousal-heading'>Ai Law Assistant</h3>
+                
                 <p>
-                    Laws Guidance website for user legal scenarios utilizing Large Language model (Bert). The feedback is taken from the user after prediction until the user didn’t gets the right answer Questions are saved in the yes or no columns against the laws that help in better prediction (reinforcement) Utilizing thread, it automatically retrains the model every 4th hour on new feedbacks making it more better This projects aims to first launch among the Lawyers that make it learn through feedbacks and then to the public
+                    Laws Guidance website for user legal scenarios utilizing Large Language model (Bert), The feedback is taken from the user after prediction
+                    until the user didn’t gets the right answer, Questions are saved in the yes or no columns against the laws that help in better prediction 
+                    (reinforcement), Utilizing thread, it automatically retrains the model every 4th hour on new feedbacks making it more better, This projects
+                    aims to first launch among the Lawyers that make it learn through feedbacks and then to the public.
                 </p>
                   <a href="http://54.151.2.29/" target='_blank'>
                 <button>
@@ -71,6 +76,9 @@ function MyCarousel() {
                 </a>
             </Carousel.Item>
             <Carousel.Item>
+
+                <h3 className='carousal-heading'>DS Templet Share for Automation <br/> ( Flask + SQLAlchemy )</h3>
+                
                 <div class="card-deck">
                     <div class="card col-md-3">
                         <img className='carousal-img' src="ds-templet1.PNG" alt="" />
@@ -88,13 +96,11 @@ function MyCarousel() {
                         <img className='carousal-img' src="ds-templet3.PNG" alt="" />
                     </div>
                 </div>
-
-
-
-
-
-                <h3 className='carousal-heading'>DS Templet Share for Automation</h3>
-                <p>Project aims to automate the data science regular tasks like EDA, Feature Extraction, Model Selection to save time Website has all the 4 steps of Data Science Automated to some extinct which can be operated by just giving some answers about dataset user want to use and automatically do EDA, feature Engineering or Model Selection</p>
+                
+                <p>Project aims to automate the data science regular tasks like EDA, Feature Extraction, Model Selection to save time, Website has all the 4 steps of Data Science
+                    Automated to some extinct which can be operated by just giving some answers about dataset user want to use and automatically do EDA, feature Engineering or 
+                    Model Selection
+                </p>
                 <a href="https://dsprojecttempletshare.pythonanywhere.com/" target='_blank'>
                 <button>
                      Check it Out <FaRocket/>
@@ -102,6 +108,10 @@ function MyCarousel() {
                 </a>
             </Carousel.Item>
             <Carousel.Item>
+
+
+                <h3 className='carousal-heading'>Expense Tracker <br/> ( Laravel + MySQL ) </h3>
+                
                 <div class="card-deck">
                     <div class="card col-md-3">
                         <img className='carousal-img' src="expense-tracker-img1.png" alt="" />
@@ -119,14 +129,10 @@ function MyCarousel() {
                         <img className='carousal-img' src="expense-tracker-img3.png" alt="" />
                     </div>
                 </div>
-
-
-
-
-
-                <h3 className='carousal-heading'>Expense Tracker</h3>
+                
                 <p>
-                    This project aims to help keep record of the user expenses to do money management User first make account in order to utilize the features, add his expenses and categories Can generate the monthly weekly and daily report category wise and also set the limit to monthly limit
+                This project aims to help keep record of the user expenses to do money management User first make account in order to utilize the features, add his expenses
+                    and categories, can generate the monthly weekly and daily report category wise and also set the limit to monthly limit
                 </p>
                 <a href="https://expensetrackeragk.000webhostapp.com/" target='_blank'>
                 <button>
